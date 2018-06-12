11558b5785ae0c0e2d867bc552da97523c1d073d
Yann Thomas-Gérard <inside@gmail.com>
2018-06-12 15:55:54 +0200

    Publish
     - @leboncoin/editorconfig@1.0.10

---
+++ packages/editorconfig/README.md
@@ -0,0 +1,3 @@
+# editorconfig
+
+Leboncoin's front web site shareable editorconfig
---
+++ packages/editorconfig/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
+const execSync = require('child_process').execSync
+const source = path.join(__dirname, '.editorconfig')
+const gitRoot = execSync('git rev-parse --show-toplevel').toString().trim()
+const dest = path.join(gitRoot, '.editorconfig')
+
+if (source === dest) {
+  console.log('lbc-editorconfig: Can\'t copy .editorconfig because ' +
+              'source and destination are identical')
+  process.exit(0)
+}
+
+console.log(`lbc-editorconfig: Will create the .editorconfig file (${dest})`)
+
+if (fs.existsSync(dest)) {
+  console.log('lbc-editorconfig: .editorconfig exists, deleting it.')
+  fs.unlinkSync(dest)
+}
+
+console.log('lbc-editorconfig: Create the .editorconfig file.')
+const data = fs.readFileSync(source)
+fs.writeFileSync(dest, data, { mode: 0o444 })
