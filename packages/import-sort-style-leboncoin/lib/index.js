dd9dd191bf32607dd2e44c11ab2ff8a7f871ef20
Thibaut Sabot <thibaut.sabot@mbp-C02X72PHJHD2.local>
2018-10-24 16:39:40 +0200

    publish with leboncoin scope
    Publish
    
     - @leboncoin/import-sort-parser-babylon-legacy@1.0.1
     - @leboncoin/import-sort-style-leboncoin@1.0.1
     - @leboncoin/internal-dev@2.0.23
     - stylelint-config-leboncoin@1.0.2
    
    manually bump version to match npm
    
    Publish
    
     - stylelint-config-leboncoin@1.0.7
    
    Publish
    
     - stylelint-config-leboncoin@1.0.7
    
    Publish
    
     - @leboncoin/internal-dev@2.0.22
     - stylelint-config-leboncoin@1.0.8
    
    Publish
    
     - @leboncoin/import-sort-parser-babylon-legacy@1.0.1
     - @leboncoin/import-sort-style-leboncoin@1.0.1
     - @leboncoin/internal-dev@2.0.23
     - stylelint-config-leboncoin@1.0.2
    
    manually bump version to match npm
    
    Publish
    
     - stylelint-config-leboncoin@1.0.7
    
    Publish
    
     - stylelint-config-leboncoin@1.0.7
    
    Publish
    
     - @leboncoin/internal-dev@2.0.22
     - stylelint-config-leboncoin@1.0.8

--- packages/import-sort-style-leboncoin/lib/index.js
+++ packages/import-sort-style-leboncoin/lib/index.js
@@ -1,61 +1,51 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function isAliasModule({ moduleName }) {
-  return moduleName.startsWith("$");
+    return moduleName.startsWith("$");
 }
 function isStyle({ moduleName }) {
-  return moduleName.endsWith(".scss") || moduleName.endsWith(".css");
+    return moduleName.endsWith(".scss") || moduleName.endsWith(".css");
 }
 function default_1(styleApi) {
-  const {
-    and,
-    dotSegmentCount,
-    hasNoMember,
-    isAbsoluteModule,
-    isNodeModule,
-    isRelativeModule,
-    member,
-    not,
-    naturally
-  } = styleApi;
-  // @ts-ignore
-  return [
-    // import "foo" with side effects
-    { match: and(hasNoMember, isAbsoluteModule) },
-    { separator: true },
-    // import "./foo" with side effects
-    { match: and(hasNoMember, isRelativeModule) },
-    { separator: true },
-    // import … from "fs";
-    {
-      match: and(isNodeModule, not(isAliasModule)),
-      sort: member(naturally)
-    },
-    { separator: false },
-    // import … from "foo";
-    {
-      match: and(isAbsoluteModule, not(isAliasModule)),
-      sort: member(naturally)
-    },
-    { separator: true },
-    // import … from "$src/";
-    {
-      match: isAliasModule,
-      sort: member(naturally)
-    },
-    { separator: true },
-    // import … from "./foo";
-    // import … from "../foo";
-    {
-      match: and(isRelativeModule, not(isStyle)),
-      sort: [dotSegmentCount, member(naturally)]
-    },
-    { separator: false },
-    // import … from "./styles";
-    {
-      match: isStyle
-    },
-    { separator: true }
-  ];
+    const { and, dotSegmentCount, hasNoMember, isAbsoluteModule, isNodeModule, isRelativeModule, member, not, naturally } = styleApi;
+    // @ts-ignore
+    return [
+        // import "foo" with side effects
+        { match: and(hasNoMember, isAbsoluteModule) },
+        { separator: true },
+        // import "./foo" with side effects
+        { match: and(hasNoMember, isRelativeModule) },
+        { separator: true },
+        // import … from "fs";
+        {
+            match: and(isNodeModule, not(isAliasModule)),
+            sort: member(naturally)
+        },
+        { separator: false },
+        // import … from "foo";
+        {
+            match: and(isAbsoluteModule, not(isAliasModule)),
+            sort: member(naturally)
+        },
+        { separator: true },
+        // import … from "$src/";
+        {
+            match: isAliasModule,
+            sort: member(naturally)
+        },
+        { separator: true },
+        // import … from "./foo";
+        // import … from "../foo";
+        {
+            match: and(isRelativeModule, not(isStyle)),
+            sort: [dotSegmentCount, member(naturally)]
+        },
+        { separator: false },
+        // import … from "./styles";
+        {
+            match: isStyle
+        },
+        { separator: true }
+    ];
 }
 exports.default = default_1;
